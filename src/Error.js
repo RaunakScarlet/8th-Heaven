9498c05139270a125e9f7dff62b5821bbfcd85ee
Raunak Scarlet <raunakscarlet@gmail.com>
2023-10-27 12:56:41 +0530

    fixed error component

--- src/Error.js
+++ src/Error.js
@@ -8,12 +8,22 @@ const Error = () => {
     console.log(err);
 
     return (
-        <div>
-            <h1>OOPS</h1>
-            <p>Something went wrong</p>
-            <h3>{err.status + " "+ err.statusText}</h3>
-
+        <div className=" flex flex-col items-center justify-center">
+            <h3 className="font-bold text-3xl p-4 m-4">
+                {err.status + " " + err.statusText}
+            </h3>
+            <h1 className="font-bold text-3xl p-4 m-4">
+                Please Open In The DeskTop Mode If You Are On Mobile View
+            </h1>
+            <button
+                className="p-2 m-2 bg-black text-white rounded-lg"
+                onClick={() => {
+                    window.location.href = "/";
+                }}
+            >
+                Back 2 Home
+            </button>
         </div>
-    )
+    );
 }
 export default Error;
